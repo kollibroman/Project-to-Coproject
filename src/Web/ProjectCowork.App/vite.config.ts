3a76302b42536577f89e37261f28c56b43d8bd9c
Filip <filipkwiek888@gmail.com>
2026-08-23 15:06:09 +0200

    add mantine and api client

--- src/Web/ProjectCowork.App/vite.config.ts
+++ src/Web/ProjectCowork.App/vite.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { heyApiPlugin } from '@hey-api/vite-plugin';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [heyApiPlugin({
+    config: {
+      input: 'hey-api/backend', // sign up at app.heyapi.dev
+      output: 'src/client',
+    },}),
+    react()],
 })
